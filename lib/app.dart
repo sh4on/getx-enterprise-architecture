6c7cf79f97c6368f21a497ca66fa96d50841f410
shaon <shaonx@proton.me>
2026-02-02 18:08:28 +0600

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const Scaffold(),
+    );
+  }
+}
